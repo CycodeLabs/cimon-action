024c1adafcad879688fa8adb3649324f1e70e270
Elad Pticha <elad@cycode.com>
2023-05-01 16:51:11 +0300

    Removed debug.

--- src/post/index.js
+++ src/post/index.js
@@ -7,7 +7,7 @@ import poll from "../poll/poll.js";
 function getActionConfig() {
     return {
         job: process.env.GITHUB_JOB,
-        createArtifact: new Boolean(core.getBooleanInput('upload-artifact')),
+        createArtifact: core.getBooleanInput('upload-artifact'),
     };
 }
 
