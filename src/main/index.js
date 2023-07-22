e574175599e41809d89baea10874f365c5d76178
Alex Ilgayev <23579644+alex-ilgayev@users.noreply.github.com>
2023-07-22 21:10:25 +0300

    refactor: remove env vars, and stop container (#40)

--- src/main/index.js
+++ src/main/index.js
@@ -2,61 +2,40 @@ import core from '@actions/core';
 import exec from '@actions/exec';
 import docker from '../docker/docker.js';
 import poll from '../poll/poll.js';
+import fs from 'fs';
 
 function getActionConfig() {
-    const dockerImage = core.getInput('docker-image');
-    const dockerImagePull = core.getBooleanInput('docker-image-pull');
-    const dockerUsername = core.getInput('docker-username');
-    const dockerPassword = core.getInput('docker-password');
-
-    const token = core.getInput('github-token');
-    const reportJobSummary = core.getBooleanInput('report-job-summary');
-
-    const logLevel = core.getInput('log-level');
-    const preventionMode = core.getBooleanInput('prevent');
-    const allowedIPs = core.getInput('allowed-ips');
-    const allowedHosts = core.getInput('allowed-hosts');
-    const ignoredIPNets = core.getInput('ignored-ip-nets');
-
-    const applyFsEvents = core.getBooleanInput('apply-fs-events');
-    const clientId = core.getInput('client-id');
-    const secret = core.getInput('secret');
-
-    const reportProcessTree = core.getBooleanInput('report-process-tree');
-    const slackWebhookEndpoint = core.getInput('slack-webhook-endpoint');
-    const featureGates = core.getMultilineInput('feature-gates');
-
     return {
         docker: {
-            image: dockerImage,
-            imagePull: dockerImagePull,
-            username: dockerUsername,
-            password: dockerPassword,
+            image: core.getInput('docker-image'),
+            imagePull: core.getBooleanInput('docker-image-pull'),
+            username: core.getInput('docker-username'),
+            password: core.getInput('docker-password'),
         },
         github: {
-            token: token,
-            jobSummary: reportJobSummary,
+            token: core.getInput('github-token'),
+            jobSummary: core.getBooleanInput('report-job-summary'),
         },
         cimon: {
-            logLevel: logLevel,
-            preventionMode: preventionMode,
-            allowedIPs: allowedIPs,
-            allowedHosts: allowedHosts,
-            ignoredIPNets: ignoredIPNets,
-            applyFsEvents: applyFsEvents,
-            clientId: clientId,
-            secret: secret,
-            featureGates: featureGates,
+            logLevel: core.getInput('log-level'),
+            preventionMode: core.getBooleanInput('prevent'),
+            allowedIPs: core.getInput('allowed-ips'),
+            allowedHosts: core.getInput('allowed-hosts'),
+            ignoredIPNets: core.getInput('ignored-ip-nets'),
+            applyFsEvents: core.getBooleanInput('apply-fs-events'),
+            clientId: core.getInput('client-id'),
+            secret: core.getInput('secret'),
+            featureGates: core.getMultilineInput('feature-gates'),
         },
         report: {
-            processTree: reportProcessTree,
-            slackWebhookEndpoint: slackWebhookEndpoint,
+            processTree: core.getBooleanInput('report-process-tree'),
+            slackWebhookEndpoint: core.getInput('slack-webhook-endpoint'),
         },
     };
 }
 
 async function run(config) {
-    if (config.docker.username !== "" && config.docker.password !== "") {
+    if (config.docker.username !== '' && config.docker.password !== '') {
         await docker.login(config.docker.username, config.docker.password);
     }
 
@@ -64,58 +43,55 @@ async function run(config) {
         await docker.imagePull(config.docker.image);
     }
 
-    const args = ['container', 'run',
+    const envOutput = await exec.getExecOutput('env', [], {
+        silent: true,
+    });
+    if (envOutput.exitCode !== 0) {
+        throw new Error(
+            `Failed fetching environment variables: ${envOutput.exitCode}: ${envOutput.stderr}`
+        );
+    }
+    fs.writeFileSync('/tmp/.env', envOutput.stdout);
+
+    const args = [
+        'container',
+        'run',
         '--detach',
-        '--name', 'cimon',
+        '--name',
+        'cimon',
         '--privileged',
         '--pid=host',
         '--network=host',
         '--cgroupns=host',
-        '--volume', '/sys/kernel/debug:/sys/kernel/debug:ro',
-        '--volume', '/home/runner/work:/github_workspace',
-        '--env', `CIMON_LOG_LEVEL=${config.cimon.logLevel}`,
-        '--env', 'GITHUB_ACTIONS=true',
-        '--env', `GITHUB_TOKEN=${config.github.token}`,
-        '--env', `GITHUB_SHA`,
-        '--env', `GITHUB_REPOSITORY`,
-        '--env', `GITHUB_REPOSITORY_ID`,
-        '--env', `GITHUB_REPOSITORY_OWNER`,
-        '--env', `GITHUB_REPOSITORY_OWNER_ID`,
-        '--env', `GITHUB_WORKFLOW`,
-        '--env', `GITHUB_WORKFLOW_REF`,
-        '--env', `GITHUB_WORKFLOW_SHA`,
-        '--env', `GITHUB_REF`,
-        '--env', `GITHUB_REF_NAME`,
-        '--env', `GITHUB_REF_PROTECTED`,
-        '--env', `GITHUB_REF_TYPE`,
-        '--env', `GITHUB_BASE_REF`,
-        '--env', `GITHUB_HEAD_REF`,
-        '--env', `GITHUB_ACTOR`,
-        '--env', `GITHUB_ACTOR_ID`,
-        '--env', `GITHUB_TRIGGERING_ACTOR`,
-        '--env', `GITHUB_JOB`,
-        '--env', `GITHUB_EVENT_NAME`,
-        '--env', `GITHUB_RUN_ID`,
-        '--env', `GITHUB_RUN_ATTEMPT`,
-        '--env', `RUNNER_ARCH`,
-        '--env', `RUNNER_NAME`,
-        '--env', `RUNNER_OS`,
+        '--volume',
+        '/sys/kernel/debug:/sys/kernel/debug:ro',
+        '--volume',
+        '/home/runner/work:/github_workspace',
+        '--env',
+        `CIMON_LOG_LEVEL=${config.cimon.logLevel}`,
+        '--env',
+        `GITHUB_TOKEN=${config.github.token}`,
+        '--env-file',
+        `/tmp/.env`,
     ];
 
     if (config.cimon.preventionMode) {
         args.push('--env', 'CIMON_PREVENT=1');
     }
 
-    if (config.cimon.allowedIPs !== "") {
+    if (config.cimon.allowedIPs !== '') {
         args.push('--env', `CIMON_ALLOWED_IPS=${config.cimon.allowedIPs}`);
     }
 
-    if (config.cimon.allowedHosts !== "") {
+    if (config.cimon.allowedHosts !== '') {
         args.push('--env', `CIMON_ALLOWED_HOSTS=${config.cimon.allowedHosts}`);
     }
 
-    if (config.cimon.ignoredIPNets !== "") {
-        args.push('--env', `CIMON_IGNORED_IP_NETS=${config.cimon.ignoredIPNets}`);
+    if (config.cimon.ignoredIPNets !== '') {
+        args.push(
+            '--env',
+            `CIMON_IGNORED_IP_NETS=${config.cimon.ignoredIPNets}`
+        );
     }
 
     if (config.github.jobSummary) {
@@ -127,25 +103,27 @@ async function run(config) {
     }
 
     if (config.report.slackWebhookEndpoint) {
-        args.push('--env', `CIMON_SLACK_WEBHOOK_ENDPOINT=${config.report.slackWebhookEndpoint}`);
+        args.push(
+            '--env',
+            `CIMON_SLACK_WEBHOOK_ENDPOINT=${config.report.slackWebhookEndpoint}`
+        );
     }
 
     if (config.cimon.applyFsEvents) {
         args.push('--env', 'CIMON_APPLY_FS_EVENTS=1');
     }
 
-    if (config.cimon.clientId !== "") {
+    if (config.cimon.clientId !== '') {
         args.push('--env', `CIMON_CLIENT_ID=${config.cimon.clientId}`);
     }
 
-    if (config.cimon.secret !== "") {
+    if (config.cimon.secret !== '') {
         args.push('--env', `CIMON_SECRET=${config.cimon.secret}`);
     }
 
-    if (config.cimon.featureGates !== "") {
+    if (config.cimon.featureGates !== '') {
         args.push('--env', `CIMON_FEATURE_GATES=${config.cimon.featureGates}`);
     }
-
 
     args.push(config.docker.image);
 
@@ -154,20 +132,32 @@ async function run(config) {
     });
 
     if (exitCode !== 0) {
-        throw new Error('Failed executing docker run command for Cimon container');
+        throw new Error(
+            'Failed executing docker run command for Cimon container'
+        );
     }
 
-    const health = await poll(async () => {
-        const state = await docker.getContainerState('cimon');
-        core.debug(`Checking Cimon health status: ${state.Health.Status} ...`);
-        return state.Health;
-    }, (health) => {
-        return health.Status !== docker.CONTAINER_STATUS_HEALTHY;
-    }, 1000, 45 * 1000);
+    fs.unlinkSync('/tmp/.env');
+
+    const health = await poll(
+        async () => {
+            const state = await docker.getContainerState('cimon');
+            core.debug(
+                `Checking Cimon health status: ${state.Health.Status} ...`
+            );
+            return state.Health;
+        },
+        (health) => {
+            return health.Status !== docker.CONTAINER_STATUS_HEALTHY;
+        },
+        1000,
+        45 * 1000
+    );
 
     if (health.Status !== docker.CONTAINER_STATUS_HEALTHY) {
         const log = health.Log;
-        let message = 'Failed reaching healthy container status for Cimon container';
+        let message =
+            'Failed reaching healthy container status for Cimon container';
         if (Array.isArray(log) && log.length > 0) {
             const latestEntry = log[0];
             message += `: exit code: ${latestEntry.ExitCode}: ${latestEntry.Output}`;
@@ -175,7 +165,9 @@ async function run(config) {
         throw new Error(message);
     }
 
-    core.info(`Build runtime security agent started successfully: ${config.docker.image}`);
+    core.info(
+        `Build runtime security agent started successfully: ${config.docker.image}`
+    );
 }
 
 try {
